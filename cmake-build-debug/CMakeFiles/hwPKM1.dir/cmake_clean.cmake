e2c69046ae8b8cc9fb8f720feff85e84dcd71baa
Joel Gutierrez <joel.gutierrez01@utrgv.edu>
2018-07-12 13:44:03 -0500

    Initial Repository

---
+++ cmake-build-debug/CMakeFiles/hwPKM1.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/hwPKM1.dir/main.cpp.obj"
+  "hwPKM1.pdb"
+  "hwPKM1.exe"
+  "hwPKM1.exe.manifest"
+  "libhwPKM1.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/hwPKM1.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
