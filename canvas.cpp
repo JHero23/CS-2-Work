ba53d5360ab9037609f9d4e316aecb36b129406e
Joel Gutierrez <joel.gutierrez01@utrgv.edu>
2018-07-16 16:42:31 -0500

    Initialized Repository.

---
+++ canvas.cpp
@@ -0,0 +1,42 @@
+//
+// Created by joelg on 7/16/2018.
+//
+
+#ifndef CANVAS_H
+#define CANVAS_H
+
+#include "canvas.h"
+
+Canvas::Canvas(int width) {
+
+}
+
+Canvas::Canvas(char x) {
+
+}
+
+Canvas::Canvas(string s) {
+
+}
+
+Canvas::~Canvas() {
+
+}
+
+int Canvas::width() {
+
+}
+
+string Canvas::to_string() {
+
+}
+
+void Canvas::replace(char old_char, char new_char) {
+
+}
+
+void Canvas::add(char x) {
+
+}
+
+#endif
