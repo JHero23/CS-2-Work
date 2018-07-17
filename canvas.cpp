edbc39b40da3457096fceb17c8d6f00896e1814b
Joel Gutierrez <joel.gutierrez01@utrgv.edu>
2018-07-17 01:52:56 -0500

    Edited canvas.cpp, still need to work on the functions.

--- canvas.cpp
+++ canvas.cpp
@@ -1,42 +1,104 @@
 //
 // Created by joelg on 7/16/2018.
 //
-
-#ifndef CANVAS_H
-#define CANVAS_H
-
+#include <iostream>
 #include "canvas.h"
 
+using namespace std;
+
+// Allocates a canvas of the given width and height 5 that
+// consists entirely of ' ' (space) chars.
 Canvas::Canvas(int width) {
-
+    _width = width;
 }
 
+// Allocates a canvas with width 5 and height 5 that looks like:
+//
+//  ###       ####        ####     ####
+// #   #      #   #      #         #   #
+// #####  or  ####   or  #      or #   #
+// #   #      #   #      #         #   #
+// #   #      ####        ####     ####
+//
+// depending upon which character ('A', 'B', 'C', or 'D') is
+// given as a parameter. If some other character is given,
+// allocates a canvas of ' ' chars with width 5 and height 5.
 Canvas::Canvas(char x) {
+    char temp = ' ';
+    C = new char*[width()];
 
+    for (int i = 0; i < width(); i++) {
+        C[i] = new char[width()];
+    }
+
+    for (int i = 0; i < width(); i++) {
+        if (x == 'A' || x == 'B' || x == 'C' || x == 'D')
+            C[i] = &x;
+        else
+            C[i] = &temp;
+        for (int j = 0; j < width(); j++) {
+            if (x == 'A' || x == 'B' || x == 'C' || x == 'D')
+                C[j] = &x;
+            else
+                C[j] = &temp;
+        }
+    }
 }
 
+// Allocates a canvas containing the sequence of characters
+// in the string with 2 columns of space between each pair
+// of adjacent characters. For instance, Canvas("BADCAB")
+// should yield:
+//
+// ####    ###   ####    ####   ###   ####
+// #   #  #   #  #   #  #      #   #  #   #
+// ####   #####  #   #  #      #####  ####
+// #   #  #   #  #   #  #      #   #  #   #
+// ####   #   #  ####    ####  #   #  ####
+//
+// Any characters in s not from {'A', 'B', 'C', 'D'} should be
+// replaced with empty 5x5 space, just like previous constructor.
 Canvas::Canvas(string s) {
 
 }
 
-Canvas::~Canvas() {
-
-}
-
+// Returns the width of the canvas.
 int Canvas::width() {
-
+    return _width;
 }
 
+// Returns the entire canvas as a single string, consisting of each row
+// of the canvas, followed by the newline character ('\n').
 string Canvas::to_string() {
+    string s;
 
 }
 
+// Replaces every instance in the canvas of old_char with new_char.
+// For instance, if old_char is '#' and new char is '@', then:
+//
+//  ###             @@@
+// #   #           @   @
+// #####  becomes  @@@@@
+// #   #           @   @
+// #   #           @   @
+//
 void Canvas::replace(char old_char, char new_char) {
+    char *temp;
+
+    temp = &old_char;
 
 }
 
+// Adds a character to the Canvas's sequence of characters.
 void Canvas::add(char x) {
 
 }
 
-#endif
+// Destructor. Deallocates all of the memory allocated by the canvas.
+Canvas::~Canvas() {
+    for (int i = 0; i < width(); i++) {
+        delete [] C[i];
+    }
+    delete [] C;
+}
--- canvas.h
+++ canvas.h
@@ -11,69 +11,69 @@ using namespace std;
 // consisting of a rectangular grid of chararacter pixels.
 class Canvas
 {
-public:
-    // Allocates a canvas of the given width and height 5 that
-    // consists entirely of ' ' (space) chars.
-    Canvas(int width);
+	public:
+		// Allocates a canvas of the given width and height 5 that
+		// consists entirely of ' ' (space) chars.
+		Canvas(int width);
 
-    // Allocates a canvas with width 5 and height 5 that looks like:
-    //
-    //  ###       ####        ####     ####
-    // #   #      #   #      #         #   #
-    // #####  or  ####   or  #      or #   #
-    // #   #      #   #      #         #   #
-    // #   #      ####        ####     ####
-    //
-    // depending upon which character ('A', 'B', 'C', or 'D') is
-    // given as a parameter. If some other character is given,
-    // allocates a canvas of ' ' chars with width 5 and height 5.
-    Canvas(char x);
+		// Allocates a canvas with width 5 and height 5 that looks like:
+		//
+		//  ###       ####        ####     ####
+		// #   #      #   #      #         #   #
+		// #####  or  ####   or  #      or #   #
+		// #   #      #   #      #         #   #
+		// #   #      ####        ####     ####
+		//
+		// depending upon which character ('A', 'B', 'C', or 'D') is
+		// given as a parameter. If some other character is given,
+		// allocates a canvas of ' ' chars with width 5 and height 5.
+		Canvas(char x);
 
-    // Allocates a canvas containing the sequence of characters
-    // in the string with 2 columns of space between each pair
-    // of adjacent characters. For instance, Canvas("BADCAB")
-    // should yield:
-    //
-    // ####    ###   ####    ####   ###   ####
-    // #   #  #   #  #   #  #      #   #  #   #
-    // ####   #####  #   #  #      #####  ####
-    // #   #  #   #  #   #  #      #   #  #   #
-    // ####   #   #  ####    ####  #   #  ####
-    //
-    // Any characters in s not from {'A', 'B', 'C', 'D'} should be
-    // replaced with empty 5x5 space, just like previous constructor.
-    Canvas(string s);
+		// Allocates a canvas containing the sequence of characters
+		// in the string with 2 columns of space between each pair
+		// of adjacent characters. For instance, Canvas("BADCAB")
+		// should yield:
+		//
+		// ####    ###   ####    ####   ###   ####
+		// #   #  #   #  #   #  #      #   #  #   #
+		// ####   #####  #   #  #      #####  ####
+		// #   #  #   #  #   #  #      #   #  #   #
+		// ####   #   #  ####    ####  #   #  ####
+		//
+		// Any characters in s not from {'A', 'B', 'C', 'D'} should be
+		// replaced with empty 5x5 space, just like previous constructor.
+		Canvas(string s);
 
-    // Returns the width of the canvas.
-    int width();
+		// Returns the width of the canvas.
+		int width();
 
-    // Returns the entire canvas as a single string, consisting of each row
-    // of the canvas, followed by the newline character ('\n').
-    string to_string();
+		// Returns the entire canvas as a single string, consisting of each row
+		// of the canvas, followed by the newline character ('\n').
+		string to_string();
 
-    // Replaces every instance in the canvas of old_char with new_char.
-    // For instance, if old_char is '#' and new char is '@', then:
-    //
-    //  ###             @@@
-    // #   #           @   @
-    // #####  becomes  @@@@@
-    // #   #           @   @
-    // #   #           @   @
-    //
-    void replace(char old_char, char new_char);
+		// Replaces every instance in the canvas of old_char with new_char.
+		// For instance, if old_char is '#' and new char is '@', then:
+		//
+		//  ###             @@@
+		// #   #           @   @
+		// #####  becomes  @@@@@
+		// #   #           @   @
+		// #   #           @   @
+		//
+		void replace(char old_char, char new_char);
 
-    // Adds a character to the Canvas's sequence of characters.
-    void add(char x);
+		// Adds a character to the Canvas's sequence of characters.
+		void add(char x);
 
-    // Destructor. Deallocates all of the memory allocated by the canvas.
-    ~Canvas();
+		// Destructor. Deallocates all of the memory allocated by the canvas.
+		~Canvas();
 
-private:
-    // A canvas is represented as a 2D char array, i.e.
-    // an array of pointers to char (sub)arrays.
-    // Each subarray corresponds to a column of the image.
-    char** C;
-    int _width;
+	private:
+		// A canvas is represented as a 2D char array, i.e.
+		// an array of pointers to char (sub)arrays.
+		// Each subarray corresponds to a column of the image.
+		char** C;
+		int _width;
 };
 
 #endif
