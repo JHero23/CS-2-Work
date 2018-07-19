7b88b9b5dd32a622ea73c2082e6624180d4c3c98
tpc345 <joel.gutierrez01@utrgv.edu>
2018-07-19 14:25:42 -0500

    Finished assignment.

--- canvas.cpp
+++ canvas.cpp
@@ -7,11 +7,11 @@
 
 using namespace std;
 
-char* a[] = { " ### ", "#   #", "#####", "#   #", "#   #",};
+char* a[] = { " ### ", "#   #", "#####", "#   #", "#   #"};
 char* b[] = {"#### ", "#   #", "#### ", "#   #", "#### "};
 char* c[] = {" ####", "#    ", "#    ", "#    ", " ####"};
 char* d[] = {"#### ", "#   #", "#   #", "#   #", "#### "};
-char* no[] = {"     ", "     ", "     ", "     ", "     ",};
+char* no[] = {"     ", "     ", "     ", "     ", "     "};
 // Allocates a canvas of the given width and height 5 that
 // consists entirely of ' ' (space) chars.
 Canvas::Canvas(int width) {
@@ -43,13 +43,12 @@ Canvas::Canvas(int width) {
 // allocates a canvas of ' ' chars with width 5 and height 5.
 Canvas::Canvas(char x) {
     _width = 5;
-    string s(_width, ' ');
 
     C = new char*[5];
 
     for (int r = 0; r < 5; r++) {
         C[r] = new char[_width + 1];
-        memset(C[r], NULL, _width + 1);
+        memset(C[r], 0, _width + 1);
         if (x == 'A') {
             strncpy(C[r], a[r], _width);
         } else if (x == 'B') {
@@ -62,13 +61,6 @@ Canvas::Canvas(char x) {
             strncpy(C[r], no[r], _width);
         }
     }
-
-    /*for (int r = 0; r < 5; r++) {
-        for (int c = 0; c < 5; c++) {
-            cout << C[r][c];
-        }
-        cout << endl;
-    }*/
 }
 
 // Allocates a canvas containing the sequence of characters
@@ -85,12 +77,21 @@ Canvas::Canvas(char x) {
 // Any characters in s not from {'A', 'B', 'C', 'D'} should be
 // replaced with empty 5x5 space, just like previous constructor.
 Canvas::Canvas(string s) {
-    _width = 5;
+    Canvas nC(s[0]);
 
-    C = new char*[_width];
-    for (int i = 0; i < _width; i++) {
-        C[i] = new char[_width];
+    for (int r = 1; r < s.length(); r++) {
+        nC.add(s[r]);
     }
+
+    C = new char*[5];
+
+    for (int r = 0; r < 5; r++) {
+        C[r] = new char[nC._width + 1];
+        memset(C[r], 0, nC._width + 1);
+        strncpy(C[r], nC.C[r], nC._width);
+    }
+
+    _width = nC._width;
 }
 
 // Returns the width of the canvas.
@@ -101,14 +102,12 @@ int Canvas::width() {
 // Returns the entire canvas as a single string, consisting of each row
 // of the canvas, followed by the newline character ('\n').
 string Canvas::to_string() {
-    string s = "";
+    string s;
 
     for (int i = 0; i < 5; i++) {
         for (int j = 0; j < _width; j++) {
-            cout << C[i][j];
             s += C[i][j];
         }
-        cout << endl;
         s += '\n';
     }
 
@@ -143,46 +142,44 @@ void Canvas::add(char x) {
 
     for (int r = 0; r < 5; r++) {
         nC[r] = new char[_width + 1];
-        memset(nC[r], '\0', _width + 1);
+        memset(nC[r], 0, _width + 1);
 
         if (x == 'A') {
             strncpy(nC[r], C[r], _width - 7);
             strncat(nC[r], "  ", 2);
-            strncpy(nC[r], a[r], 5);
+            strncat(nC[r], a[r], 5);
         } else if (x == 'B') {
             strncpy(nC[r], C[r], _width - 7);
             strncat(nC[r], "  ", 2);
-            strncpy(nC[r], b[r], 5);
+            strncat(nC[r], b[r], 5);
         } else if (x == 'C') {
             strncpy(nC[r], C[r], _width - 7);
             strncat(nC[r], "  ", 2);
-            strncpy(nC[r], c[r], 5);
+            strncat(nC[r], c[r], 5);
         } else if (x == 'D') {
             strncpy(nC[r], C[r], _width - 7);
             strncat(nC[r], "  ", 2);
-            strncpy(nC[r], d[r], 5);
+            strncat(nC[r], d[r], 5);
         } else {
             strncpy(nC[r], C[r], _width - 7);
             strncat(nC[r], "  ", 2);
-            strncpy(nC[r], no[r], 5);
+            strncat(nC[r], no[r], 5);
         }
     }
 
     for (int r = 0; r < 5; r++) {
-        delete [] C[r];
+        delete C[r];
     }
+
+    delete C;
 
     C = nC;
-
-    for (int r = 0; r < 5; r++) {
-        delete [] nC[r];
-    }
 }
 
 // Destructor. Deallocates all of the memory allocated by the canvas.
 Canvas::~Canvas() {
-    for (int i = 0; i < _width; i++) {
-        delete [] C[i];
+    for (int i = 0; i < 5; i++) {
+        delete C[i];
     }
-    delete [] C;
+    delete C;
 }
