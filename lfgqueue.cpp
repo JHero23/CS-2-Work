ccf0b93964d559d4123c0eb28776b4a31cb926d1
tpc345 <joel.gutierrez01@utrgv.edu>
2018-07-30 11:57:31 -0500

    Initialized Remote.

---
+++ lfgqueue.cpp
@@ -0,0 +1,36 @@
+//
+// Created by tpc345 on 7/30/2018.
+//
+#include "lfgqueue.h"
+
+using namespace std;
+
+// All of the methods are the same
+// as in hwLFG1.
+LFGQueue::LFGQueue() {
+
+}
+
+int LFGQueue::size() {
+
+}
+
+void LFGQueue::push_player(Player* p) {
+
+}
+
+Player* LFGQueue::front_player(Player::Role r) {
+
+}
+
+void LFGQueue::pop_player(Player::Role r) {
+
+}
+
+bool LFGQueue::front_group(Player** group) {
+
+}
+
+void LFGQueue::pop_group() {
+
+}
---
+++ lfgqueue.h
@@ -0,0 +1,42 @@
+
+#ifndef LFGQUEUE_H
+#define LFGQUEUE_H
+
+#include "player.h"
+
+class LFGQueue
+{
+public:
+    // All of the methods are the same
+    // as in hwLFG1.
+    LFGQueue();
+
+    int size();
+    void push_player(Player* p);
+
+    Player* front_player(Player::Role r);
+    void pop_player(Player::Role r);
+
+    bool front_group(Player** group);
+    void pop_group();
+
+private:
+    class Node
+    {
+    public:
+        Player* p;
+        Node* next;
+    };
+
+    // You can index into these arrays using Player::Role values!
+    // Check out player.h to see the int value of each Role.
+    //
+    // This lets you do things like:
+    // "if (heads[p->role()] == nullptr)" and "++counts[p->role()]"
+    Node* heads[3];
+    Node* tails[3];
+    int counts[3];
+};
+
+#endif
+
