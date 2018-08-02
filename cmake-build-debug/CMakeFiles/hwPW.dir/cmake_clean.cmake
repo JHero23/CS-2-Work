1ab2abea75af9ef612e481ac9ff8a6b4e691d118
Joel Gutierrez <joel.gutierrez01@utrgv.edu>
2018-08-02 02:03:12 -0500

    Initial Repository

---
+++ cmake-build-debug/CMakeFiles/hwPW.dir/cmake_clean.cmake
@@ -0,0 +1,14 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/hwPW.dir/main.cpp.obj"
+  "CMakeFiles/hwPW.dir/evilcomputer.cpp.obj"
+  "CMakeFiles/hwPW.dir/hack.cpp.obj"
+  "hwPW.pdb"
+  "hwPW.exe"
+  "hwPW.exe.manifest"
+  "libhwPW.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/hwPW.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ cmake-build-debug/CMakeFiles/hwPW.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for hwPW.
+# This may be replaced when dependencies are built.
