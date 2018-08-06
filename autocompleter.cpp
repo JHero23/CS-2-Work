f4c27ce497650f6f20df6579eca6920506abff57
tpc345 <joel.gutierrez01@utrgv.edu>
2018-08-06 14:21:27 -0500

    Working on functions in autocompleter.cpp.

--- autocompleter.cpp
+++ autocompleter.cpp
@@ -9,6 +9,7 @@ using namespace std;
 Autocompleter::Autocompleter() {
     count = 0;
     capacity = 1;
+    A = new string[capacity];
     for (int i = 0; i < capacity; i++) {
         A[i] = nullptr;
     }
@@ -19,12 +20,36 @@ Autocompleter::Autocompleter() {
 //
 // Must run in O(n) time.
 void Autocompleter::insert(string x) {
+    int index = index_of(x, A, capacity);
 
+    if (A[index] == x) {
+        return;
+    } else {
+        if (count == capacity) {
+            capacity *= 2;
+            string* new_A = new string[capacity];
+
+            for (int i = 0; i < capacity; i++) {
+                if (i < count) {
+                    new_A[i] = A[i];
+                } else {
+                    new_A[i] = nullptr;
+                }
+            }
+
+            delete A;
+            A = new_A;
+        }
+    }
+
+
+    A[count] = x;
+    count++;
 }
 
 // Returns the number of strings in the Autocompleter.
 int Autocompleter::size() {
-
+    return count;
 }
 
 // Returns the number of strings that are completions of
@@ -46,4 +71,34 @@ int Autocompleter::completion_count(string x) {
 // Must run in O(log(n)) time.
 void Autocompleter::completions(string x, string* suggestions) {
 
+}
+
+// Optional helper method.
+//
+// Assumes A is sorted.
+// If x is in A, returns the index of A containing x.
+// Otherwise, returns the index of A where x would be
+// after calling add(x).
+//
+// Should run in O(log(n)) time (use binary search).
+int Autocompleter::index_of(string x, string *A, int n) {
+    int l, m, r;
+
+    l = 0;
+    r = n - 1;
+    while (l <= r)
+    {
+        m = (l + r) / 2;
+        // Case 1: A[m] == x
+        if (A[m] == x)
+            return m;
+        // Case 2: A[m] < x
+        if (A[m] < x)
+            l = m + 1;
+            // Case 3: A[m] > x
+        else if (A[m] > x)
+            r = m - 1;
+    }
+
+    return m;
 }
